2fa018d2cf52670b217a5e1e97ff43272ce09fcc
Nikita <nikitamasslennikov@mail.ru>
2024-03-22 15:30:54 +0300

    Initial commit

---
+++ src/main/java/com/example/idf_test_task/IdfTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.idf_test_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdfTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdfTestTaskApplication.class, args);
+    }
+
+}
